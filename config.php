3063db3f040c5984f488c37b1c6ee6060d3f63df
FERDZ Dev <dev@ferdz.com.br>
2025-11-24 11:32:05 -0300

    Configurar Gmail como provedor de email principal

--- config.php
+++ config.php
@@ -7,35 +7,36 @@
 // 2. Gere uma senha de app
 // 3. Substitua '' pela senha gerada abaixo
 //
-// OPÇÃO 2: Gmail (recomendado se Outlook não funcionar)
+// OPÇÃO 2: Gmail (recomendado - MAIS FÁCIL)
 // 1. Acesse https://myaccount.google.com/security
-// 2. Ative a verificação em 2 etapas
+// 2. Ative a VERIFICAÇÃO EM 2 ETAPAS (obrigatório)
 // 3. Vá para https://myaccount.google.com/apppasswords
-// 4. Gere senha para "Point Tracing"
-// 5. Use as configurações do Gmail abaixo
+// 4. Selecione "Outro" e digite "Point Tracing"
+// 5. COPIE A SENHA DE 16 CARACTERES gerada
+// 6. Cole no campo 'password' abaixo
+// 7. DESCOMENTE as configurações do Gmail
 
 return [
     'smtp' => [
-        // Configurações Outlook/Hotmail
+        // Configurações Gmail (recomendado)
+        'host' => 'smtp.gmail.com',
+        'username' => 'ferdzagenciadigital@gmail.com',
+        'password' => '', // <-- PRECISA GERAR SENHA DE APP NO GMAIL
+        'port' => 587,
+        'encryption' => 'tls'
+
+        // Configurações Outlook/Hotmail (comentadas)
+        /*
         'host' => 'smtp-mail.outlook.com',
         'username' => 'fernandodev@hotmail.com',
         'password' => '', // <-- COLOQUE A SENHA DE APP AQUI
         'port' => 587,
         'encryption' => 'tls'
-
-        // Se usar Gmail, descomente as linhas abaixo e comente as acima:
-        /*
-        'host' => 'smtp.gmail.com',
-        'username' => 'seuemail@gmail.com',
-        'password' => '', // <-- SENHA DE APP DO GMAIL
-        'port' => 587,
-        'encryption' => 'tls'
         */
     ],
     'from' => [
-        'email' => 'fernandodev@hotmail.com',
+        'email' => 'ferdzagenciadigital@gmail.com',
         'name' => 'Point Tracing'
-        // Se usar Gmail: 'email' => 'seuemail@gmail.com'
     ],
     'to' => [
         'email' => 'fernandodev@hotmail.com',
